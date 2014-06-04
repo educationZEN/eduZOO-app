34748331cea21708b8d0cc4b7f3bd9cd521e32ba
Jörg Richter <jri@deepamehta.de>
2014-06-04 16:34:06 +0200

    Author support: integrate Google Analytics.
    Google Analytics is embedded in each eduZOO page automatically.
    The Author is not required to do anything special.

--- script/eduZOO.js
+++ script/eduZOO.js
@@ -145,4 +145,17 @@ angular.module("eduZOO", [])
 .run(function(eduUtils) {
     eduUtils.loadStylesheet("eduZOO/css/eduZOO.css")
     eduUtils.loadScript("eduZOO-app/script/lib/MathJax/MathJax.js?config=TeX-AMS_HTML")
+    //
+    addGoogleAnalytics()
+
+    function addGoogleAnalytics() {
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+        //
+        var WEB_PROPERTY_ID = "UA-51564469-1"
+        ga('create', WEB_PROPERTY_ID, 'auto');  // create tracking object
+        ga('send', 'pageview');                 // track pageview
+    }
 })
