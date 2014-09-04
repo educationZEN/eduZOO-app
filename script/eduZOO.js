20dbd7c1ba74873d2b073edd28f02228aa4eb0e3
eduzenteam <eduzenteam@gmail.com>
2014-09-04 15:02:36 +0200

    test5

--- script/eduZOO.js
+++ script/eduZOO.js
@@ -175,7 +175,6 @@ angular.module("eduZOO", [])
             var div = document.createElement("div")
             div.setAttribute("class", "fb-comments")
             div.setAttribute("data-href", location.href)
-            div.style.text-align = 'center';
             document.body.appendChild(div)
         }
     }
